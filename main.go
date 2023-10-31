ca3114f9038abfe05dbe7ecf42477996d473cdfb
SGGM <gleb_somov@mail.ru>
2023-10-31 18:32:47 +0300

    worked well, than suddenly returned  502 Bad Gateway

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 
 type Obj map[string]any
 
-func main() {
+var API = "https://cyberleninka.ru/api/search"
 
-	API := "https://cyberleninka.ru/api/search"
+func main() {
 
 	post_body, err := json.Marshal(Obj{
 		"mode": "articles",
@@ -29,5 +29,73 @@ func main() {
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Api returned a non-200 status code:", resp.Status)
+		return
+	}
 	fmt.Println("response status:", resp.Status)
+
+	var responseValidJSON map[string]interface{}
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&responseValidJSON); err != nil {
+		fmt.Println("Error decoding JSON response", err)
+		return
+	}
+
+	foundValue, found := responseValidJSON["found"]
+	if found {
+		fmt.Println("Number of papers found:", foundValue)
+	} else {
+		fmt.Println("Key 'found' not found in the response")
+	}
+
+	// Second request
+
+	// post_body2, err := json.Marshal(Obj{
+	// 	"mode": "articles",
+	// 	"size": foundValue,
+	// 	"q":    "golang",
+	// 	"from": 0,
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// resp2, err := http.Post(API, "application/json", bytes.NewReader(post_body2))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp2.Body.Close()
+
+	// if resp2.StatusCode != http.StatusOK {
+	// 	fmt.Println("Api returned a non-200 status code:", resp2.Status)
+	// 	return
+	// }
+	// fmt.Println("response status:", resp2.Status)
+
+	// var responseValidJSON2 map[string]interface{}
+	// decoder2 := json.NewDecoder(resp2.Body)
+	// if err := decoder2.Decode(&responseValidJSON2); err != nil {
+	// 	fmt.Println("Error decoding JSON response", err)
+	// 	return
+	// }
+
+	// articles := []map[string]interface{}{}
+	// numberOfPapers := foundValue
+	// var numberOfPapers any
+
+	// fmt.Println(foundValue)
+
+	// for articleNum := 0; articleNum < number; articleNum++ {
+	// 	currArticle := map[string]interface{}{}
+
+	// 	currArticle["name"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["name"]
+	// 	currArticle["annotation"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["annotation"]
+	// 	currArticle["authors"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["authors"]
+	// 	currArticle["year"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["year"]
+
+	// 	articles = append(articles, currArticle)
+	// }
+
+	// fmt.Println(articles)
 }
