4fdabe6a73dd6f9f3504f1e1c4927855a6dc64d8
Somov Gleb <gleb_somov@mail.ru>
2023-11-29 18:31:59 +0300

    Update main.go

--- main.go
+++ main.go
@@ -49,53 +49,53 @@ func main() {
 		fmt.Println("Key 'found' not found in the response")
 	}
 
-	// Second request
+	//Second request
 
-	// post_body2, err := json.Marshal(Obj{
-	// 	"mode": "articles",
-	// 	"size": foundValue,
-	// 	"q":    "golang",
-	// 	"from": 0,
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
+	post_body2, err := json.Marshal(Obj{
+		"mode": "articles",
+		"size": foundValue,
+		"q":    "golang",
+		"from": 0,
+	})
+	if err != nil {
+		panic(err)
+	}
 
-	// resp2, err := http.Post(API, "application/json", bytes.NewReader(post_body2))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer resp2.Body.Close()
+	resp2, err := http.Post(API, "application/json", bytes.NewReader(post_body2))
+	if err != nil {
+		panic(err)
+	}
+	defer resp2.Body.Close()
 
-	// if resp2.StatusCode != http.StatusOK {
-	// 	fmt.Println("Api returned a non-200 status code:", resp2.Status)
-	// 	return
-	// }
-	// fmt.Println("response status:", resp2.Status)
+	if resp2.StatusCode != http.StatusOK {
+		fmt.Println("Api returned a non-200 status code:", resp2.Status)
+		return
+	}
+	fmt.Println("response status:", resp2.Status)
 
-	// var responseValidJSON2 map[string]interface{}
-	// decoder2 := json.NewDecoder(resp2.Body)
-	// if err := decoder2.Decode(&responseValidJSON2); err != nil {
-	// 	fmt.Println("Error decoding JSON response", err)
-	// 	return
-	// }
+	var responseValidJSON2 map[string]interface{}
+	decoder2 := json.NewDecoder(resp2.Body)
+	if err := decoder2.Decode(&responseValidJSON2); err != nil {
+		fmt.Println("Error decoding JSON response", err)
+		return
+	}
 
-	// articles := []map[string]interface{}{}
-	// numberOfPapers := foundValue
-	// var numberOfPapers any
+	articles := []map[string]interface{}{}
+	numberOfPapers := foundValue
+	var numberOfPapers any
 
-	// fmt.Println(foundValue)
+	fmt.Println(foundValue)
 
-	// for articleNum := 0; articleNum < number; articleNum++ {
-	// 	currArticle := map[string]interface{}{}
+	for articleNum := 0; articleNum < number; articleNum++ {
+		currArticle := map[string]interface{}{}
 
-	// 	currArticle["name"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["name"]
-	// 	currArticle["annotation"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["annotation"]
-	// 	currArticle["authors"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["authors"]
-	// 	currArticle["year"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["year"]
+		currArticle["name"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["name"]
+		currArticle["annotation"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["annotation"]
+		currArticle["authors"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["authors"]
+		currArticle["year"] = responseValidJSON["articles"].([]interface{})[articleNum].(map[string]interface{})["year"]
 
-	// 	articles = append(articles, currArticle)
-	// }
+		articles = append(articles, currArticle)
+	}
 
-	// fmt.Println(articles)
+	fmt.Println(articles)
 }
